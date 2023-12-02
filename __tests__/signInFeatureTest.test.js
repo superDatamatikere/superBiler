dbc0a12f8a7cf65a31b51d32fe3bd5b510e16a58
Wrenchx <wzs84ahh@gmail.com>
2023-12-02 03:55:49 +0100

    Better visibility over unit and feature tests

---
+++ __tests__/signInFeatureTest.test.js
@@ -0,0 +1,19 @@
+const request = require('supertest');
+const app = require('../app'); // Replace '../app' with the path to your Express app
+
+describe('POST /', () => {
+  it('should login successfully with valid credentials', async () => {
+    const validUser = {
+      email: 'testUser@gmail.com',
+      psw: '0000' 
+    };
+
+    const response = await request(app)
+      .post('/signIn') 
+      .send(validUser);
+
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Logged in successfully');
+  });
+});
+
