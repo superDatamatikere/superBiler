5527b3f762cf66df6a89943f76564b4ea62fd304
Lau Breilev <lauxsecure@gmail.com>
2023-12-07 14:08:49 +0100

    Pug form : router post search JSON

--- routes/index.js
+++ routes/index.js
@@ -7,26 +7,36 @@ const MOT_URL = 'https://corsproxy.io/?https://www.tjekbil.dk/api/v3/tstyr/repor
 // Dette kører igennem en corsproxy, da jeg fik en CORS fejl.
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/', async function(req, res) {
 
- // res.render('index', { title: 'Express' });
-
-  plate = "ab12345"
-
-let request = await fetch(`${API_URL}/${plate}?amount=1`);
-let json;
-try {
-    json = await request.json();
-} catch(err) {
-    return $('#vehicle').html(`<h3>Der kunne ikke findes noget data på dette køretøj.<h3/>`);
-}
-
-//res.render('index', { title: 'Express', json});
-
-res.status(200).json({ json });
+  res.render('index', { title: 'Express' });
 
 });
 
+router.post('/', async function(req, res){
+  try {
+
+    const {nummerplade} = req.body;
+    plate = nummerplade;
+
+
+    if (plate == undefined) {
+        plate = nummerplade;
+    }
+
+    let request = await fetch(`${API_URL}/${plate}?amount=1`);
+    let json;
+    try {
+        json = await request.json();
+    } catch(err) {
+        return $('#vehicle').html(`<h3>Der kunne ikke findes noget data på dette køretøj.<h3/>`);
+    }
+    
+    res.status(200).json({ json });
+
+  } catch (error) { console.error('Error', error) 
+  res.status(500).json({error: 'server error'})}
+});
 
 
 module.exports = router;
