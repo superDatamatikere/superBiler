45ad7e1ecf1587e926c5c855fa2620a68a17148e
Wrenchx <wzs84ahh@gmail.com>
2023-12-10 00:49:02 +0100

    Added more error handling regarding user auth

--- routes/user.js
+++ routes/user.js
@@ -1,53 +1,62 @@
 var express = require('express');
 var router = express.Router();
-const { car, user, userCar} = require("../models")
+const { car, user, userCar } = require("../models")
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-	res.redirect('/user/account');
+router.get('/', function (req, res, next) {
+	if (!req.session.userId) {
+		res.redirect('/auth');
+	} else if (req.session.userId) {
+		res.redirect('/user/account');
+	}
 });
 
-router.get('/account', async function(req, res, next) {
+router.get('/account', async function (req, res, next) {
 	if (!req.session.userId) {
 		res.redirect('/auth');
 	}
 	else if (req.session.userId) {
-		const currentUser = await user.findOne({ where: { id: req.session.userId } });   
+		const currentUser = await user.findOne({ where: { id: req.session.userId } });
 		res.render('account', { title: 'Account Page', isLoggedIn: req.session.userId, User: currentUser });
 	}
-  });
-  
-  router.get('/update', function(req, res, next) {
-	res.send('respond with a resource');
-  });
+});
 
-  router.get('/delete', function(req, res, next) {
+router.get('/update', function (req, res, next) {
 	res.send('respond with a resource');
-  });
+});
 
-router.get('/favourite/cars', async function(req, res, next) {
+router.get('/delete', function (req, res, next) {
+	res.send('respond with a resource');
+});
+
+router.get('/favourite/cars', async function (req, res, next) {
 	try {
-	  const userCars = await userCar.findAll({ 
-		where: { userId: req.session.userId },
-		include: [car, user]
-	  });
+		if (!req.session.userId) {
+			res.redirect('/auth');
+			
+		} else if (req.session.userId) {
+			const userCars = await userCar.findAll({
+				where: { userId: req.session.userId },
+				include: [car, user]
+			});
 
-	  const formattedData = userCars.map(({ id, car, user }) => ({
-		userCarId: id,
-		userId: user?.id,
-		userName: user ? `${user.fname} ${user.lname}` : 'Unknown',
-		carId: car?.id,
-		licensePlate: car?.licensePlate
-	  }));
-  
-	  res.render('favouriteCars', { title: 'Favourite Car Relationships', userCars: formattedData });
+			const formattedData = userCars.map(({ id, car, user }) => ({
+				userCarId: id,
+				userId: user?.id,
+				userName: user ? `${user.fname} ${user.lname}` : 'Unknown',
+				carId: car?.id,
+				licensePlate: car?.licensePlate
+			}));
+
+			res.render('favouriteCars', { title: 'Favourite Car Relationships', userCars: formattedData });
+		}
 	} catch (e) {
-	  console.error(e);
-	  res.status(500).send("Error occurred");
+		console.error(e);
+		res.status(500).send("Error occurred");
 	}
-  });
+});
 
-  router.get('/cars', async function (req, res, next) {
+router.get('/cars', async function (req, res, next) {
 	try {
 		const cars = await car.findAll();
 
@@ -56,19 +65,19 @@ router.get('/favourite/cars', async function(req, res, next) {
 		console.error(e);
 		res.status(500).send("Error occurred");
 	}
-  });
+});
 
 
-  router.post('/cars', async function (req, res, next) {
+router.post('/cars', async function (req, res, next) {
 	try {
 		await userCar.create({
-			userId: req.session.userId, 
+			userId: req.session.userId,
 		});
 	} catch (e) {
 		console.error(e);
 		res.status(500).send("Error occurred");
 	}
-  });
-  
+});
+
 
 module.exports = router;
